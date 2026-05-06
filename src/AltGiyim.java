4959097749b9d7af1e341bda652c3c43e3589e89
Hilal-Cam <hilalcam.eng@gmail.com>
2026-05-06 23:12:10 +0300

    feat: add AltGiyim class as a concrete product

--- src/AltGiyim.java
+++ src/AltGiyim.java
@@ -1,1 +1,12 @@
+package src;
 
+public class AltGiyim extends Kiyafet {
+    public AltGiyim(String ad, double fiyat) {
+        super(ad, fiyat);
+    }
+
+    @Override
+    public String toString() {
+        return "[Alt Giyim] " + getAd() + " - " + getFiyat() + " TL";
+    }
+}
