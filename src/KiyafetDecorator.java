bd0c220b320ad3951d8c10c26f994588e871af23
Hilal-Cam <hilalcam.eng@gmail.com>
2026-05-09 14:53:16 +0300

    Add KiyafetDecorator abstract class implementation

--- src/KiyafetDecorator.java
+++ src/KiyafetDecorator.java
@@ -1,1 +1,12 @@
+public abstract class KiyafetDecorator implements Kiyafet {
+    protected Kiyafet dekoreEdilenKiyafet;
 
+    public KiyafetDecorator(Kiyafet kiyafet) {
+        this.dekoreEdilenKiyafet = kiyafet;
+    }
+
+    @Override
+    public double getFiyat() {
+        return dekoreEdilenKiyafet.getFiyat();
+    }
+}
