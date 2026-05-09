d05c81b27f5ab178574069350a5fedb54df41647
Hilal-Cam <hilalcam.eng@gmail.com>
2026-05-09 14:55:50 +0300

    Remove comment regarding method name mismatch
    Removed comment about method name mismatch.

--- src/DisOdemeServisi.java
+++ src/DisOdemeServisi.java
@@ -1,6 +1,6 @@
 
 public class DisOdemeServisi {
-    // Bu metod senin sistemindeki metod isimlerine (odemeYap) uymuyor
+  
     public void servisAraciligiylaOdemeAl(double miktar) {
         System.out.println(miktar + " TL dış servis üzerinden başarıyla alındı.");
     }
