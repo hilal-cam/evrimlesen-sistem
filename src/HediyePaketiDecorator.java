d79a4fa34bacc41b7a3b695e64c7cea1f5005a2b
Hilal-Cam <hilalcam.eng@gmail.com>
2026-05-09 14:54:07 +0300

    Add HediyePaketiDecorator to enhance Kiyafet pricing

--- src/HediyePaketiDecorator.java
+++ src/HediyePaketiDecorator.java
@@ -1,1 +1,10 @@
+public class HediyePaketiDecorator extends KiyafetDecorator {
+    public HediyePaketiDecorator(Kiyafet kiyafet) {
+        super(kiyafet);
+    }
 
+    @Override
+    public double getFiyat() {
+        return super.getFiyat() + 30.0;
+    }
+}
