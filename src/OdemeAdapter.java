f0fd73ccfa09301b78fb9ea33528bf3b00528b9f
Hilal-Cam <hilalcam.eng@gmail.com>
2026-05-09 14:56:23 +0300

    Update OdemeAdapter.java

--- src/OdemeAdapter.java
+++ src/OdemeAdapter.java
@@ -1,1 +1,11 @@
+public class OdemeAdapter { 
+    private DisOdemeServisi disServis;
 
+    public OdemeAdapter() {
+        this.disServis = new DisOdemeServisi();
+    }
+
+    public void odemeYap(double miktar) {
+        disServis.servisAraciligiylaOdemeAl(miktar);
+    }
+}
