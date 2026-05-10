c9bc20ed5fa3a0072aeeb5f075b674e2a537e390
Hilal-Cam <hilalcam.eng@gmail.com>
2026-05-10 20:46:44 +0300

    Implement StokGozlemcisi class with update notification

--- src/StokGozlemcisi.java
+++ src/StokGozlemcisi.java
@@ -1,1 +1,14 @@
+package src;
 
+public class StokGozlemcisi implements IStokGozlemcisi {
+    private String isim;
+
+    public StokGozlemcisi(String isim) {
+        this.isim = isim;
+    }
+
+    @Override
+    public void guncellemeAl(String urunAdi, int yeniStok) {
+        System.out.println(">>> BİLDİRİM [" + isim + "]: Beklediğiniz " + urunAdi + " stoğumuza girdi! Mevcut stok: " + yeniStok);
+    }
+}
