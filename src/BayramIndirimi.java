97e99e1606fe2107fa62aae55c2f918f7b332726
Hilal-Cam <hilalcam.eng@gmail.com>
2026-05-10 20:44:53 +0300

    Implement BayramIndirimi class for discount strategy

--- src/BayramIndirimi.java
+++ src/BayramIndirimi.java
@@ -1,1 +1,9 @@
+package src;
 
+public class BayramIndirimi implements IndirimStratejisi {
+    @Override
+    public double indirimUygula(double fiyat) {
+        System.out.println("[STRATEJİ]: Bayram indirimi (%15) uygulandı.");
+        return fiyat * 0.85; 
+    }
+}
