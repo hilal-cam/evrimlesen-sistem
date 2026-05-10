c1135c7f73eb43dffccf91793b24634528e35ad1
Hilal-Cam <hilalcam.eng@gmail.com>
2026-05-10 20:45:17 +0300

    Implement student discount strategy in OgrenciIndirimi

--- src/OgrenciIndirimi.java
+++ src/OgrenciIndirimi.java
@@ -1,1 +1,9 @@
+package src;
 
+public class OgrenciIndirimi implements IndirimStratejisi {
+    @Override
+    public double indirimUygula(double fiyat) {
+        System.out.println("[STRATEJİ]: Öğrenci indirimi (50 TL) uygulandı.");
+        return fiyat - 50.0;
+    }
+}
